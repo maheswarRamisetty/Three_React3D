cbdf269671f3744ff806dd7985af1f58872cf223
maheswarRamisetty <106472608+maheswarRamisetty@users.noreply.github.com>
2024-05-17 12:13:17 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,16 @@
 
+// src/App.tsx
+import React from 'react';
+import ModelViewer from './ModelViewer';
+
+const App = () => {
+  const modelUrl = '/path/to/your/model.gltf'; //use the above one furnitue_set.glb
+
+  return (
+    <div className="App">
+      <ModelViewer modelUrl={modelUrl} />
+    </div>
+  );
+};
+
+export default App;
